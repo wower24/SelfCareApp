93e7222d827cd732f94fabdd474994abb32f31d2
Marta Wower <marta.wower24@gmail.com>
2025-03-27 21:38:22 -0600

    Update build.gradle.kts
    Added a packaging option I forgot about earlier.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
     buildFeatures {
         compose = true
     }
+
+    packagingOptions {
+        resources.excludes.add("META-INF/atomicfu.kotlin_module")
+    }
 }
 
 dependencies {
