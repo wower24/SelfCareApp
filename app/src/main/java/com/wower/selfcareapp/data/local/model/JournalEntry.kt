61fe283cd5f2b9222578a7ded4929cd21ea84f2e
Marta Wower <marta.wower24@gmail.com>
2025-03-27 21:19:29 -0600

    JournalEntry Entity
    Created a class for Journal Entry table

---
+++ app/src/main/java/com/wower/selfcareapp/data/local/model/JournalEntry.kt
@@ -0,0 +1,13 @@
+package com.wower.selfcareapp.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "journal_entries")
+data class JournalEntry (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = -1,
+    var prompt: String = "",
+    var content: String = "",
+    var date: String = ""
+)
