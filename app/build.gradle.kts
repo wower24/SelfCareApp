5c495ced0f318b7acbb6a23df2b8291e2c6c0e49
Marta Wower <marta.wower24@gmail.com>
2025-04-01 15:41:55 -0600

    JournalEntryScreen
    Added JournalEntryScreen with an option to navigate back and add an entry.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,8 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 }
 
 fun DependencyHandlerScope.kapt(dependency:
--- app/src/main/java/com/wower/selfcareapp/presentation/journal_entry/JournalEntryScreen.kt
+++ app/src/main/java/com/wower/selfcareapp/presentation/journal_entry/JournalEntryScreen.kt
@@ -1,2 +1,145 @@
 package com.wower.selfcareapp.presentation.journal_entry
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun JournalEntryScreen(
+    modifier: Modifier = Modifier,
+    entryId: Int,
+    assistedFactory: JournalEntryViewModelAssistedFactory,
+    navigateUp: () -> Unit
+) {
+    val viewModel = viewModel(
+        modelClass = JournalEntryViewModel::class.java,
+        factory = JournalEntryViewModelFactory(
+            entryId = entryId,
+            assistedFactory = assistedFactory
+        )
+    )
+
+    val state = viewModel.state
+
+    JournalEntryScreen(
+        modifier = modifier,
+        prompt = state.prompt,
+        content = state.content,
+        isFormNotBlank = viewModel.isFormNotBlank,
+        onContentChange = viewModel::onContentChange,
+        onButtonClick = {
+            viewModel.addEntry()
+        },
+        onNavigate = navigateUp
+    )
+
+}
+
+@Composable
+private fun JournalEntryScreen(
+    modifier: Modifier,
+    prompt: String,
+    content: String,
+    isFormNotBlank: Boolean,
+    onContentChange: (String) -> Unit,
+    onButtonClick: () -> Unit,
+    onNavigate: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        TopSection(
+            modifier = modifier,
+            prompt = prompt,
+            onNavigate = onNavigate
+        )
+
+        Spacer(modifier = Modifier.Companion.size(12.dp))
+
+        AnimatedVisibility(isFormNotBlank) {
+            Row(
+                modifier = modifier.fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(onClick = onButtonClick) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Add Entry"
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.Companion.size(12.dp))
+
+        EntryTextField(
+            modifier = modifier.weight(1f),
+            value = content,
+            onValueChange = onContentChange
+        )
+    }
+}
+
+@Composable
+private fun TopSection(
+    modifier: Modifier,
+    prompt: String,
+    onNavigate: () -> Unit
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = onNavigate) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Go Back"
+            )
+        }
+
+        Text(
+            modifier = modifier.weight(1f),
+            text = prompt,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+private fun EntryTextField(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = {
+            Text(
+                text = "Enter your thoughts on that here...",
+                modifier = modifier.fillMaxWidth()
+                )
+        }
+    )
+}
