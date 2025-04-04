42bf9d5a926b31ccb60ae63466c2cf500d859d93
Marta Wower <marta.wower24@gmail.com>
2025-04-04 15:20:09 -0600

    Journal Entry Dao
    Added Journal Entry Dao with methods to get all entries, get entry by id, insert and delete

---
+++ app/src/main/java/com/wower/selfcareapp/feature_journal/data/data_source/JournalEntryDao.kt
@@ -0,0 +1,24 @@
+package com.wower.selfcareapp.feature_journal.data.data_source
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.wower.selfcareapp.feature_journal.domain.model.JournalEntry
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JournalEntryDao {
+    @Query("SELECT * FROM journal_entries")
+    fun getAllEntries(): Flow<List<JournalEntry>>
+
+    @Query("SELECT * FROM journal_entries WHERE id = :id")
+    suspend fun getEntryById(id: Int): JournalEntry?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEntry(entry: JournalEntry)
+
+    @Delete
+    suspend fun deleteEntry(entry: JournalEntry)
+}
---
+++ app/src/main/java/com/wower/selfcareapp/feature_journal/data/data_source/SelfCareDatabase.kt
@@ -0,0 +1,4 @@
+package com.wower.selfcareapp.feature_journal.data.data_source
+
+class SelfCareDatabase {
+}
