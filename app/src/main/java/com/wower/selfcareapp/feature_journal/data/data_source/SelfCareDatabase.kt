eaa7e3f1f218ebc12634967690d14f00e6db0d2c
Marta Wower <marta.wower24@gmail.com>
2025-04-04 15:22:10 -0600

    Database abstract class
    Added database abstract class

--- app/src/main/java/com/wower/selfcareapp/feature_journal/data/data_source/SelfCareDatabase.kt
+++ app/src/main/java/com/wower/selfcareapp/feature_journal/data/data_source/SelfCareDatabase.kt
@@ -1,4 +1,14 @@
 package com.wower.selfcareapp.feature_journal.data.data_source
 
-class SelfCareDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.wower.selfcareapp.feature_journal.domain.model.JournalEntry
+
+@Database(
+    entities = [JournalEntry::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class SelfCareDatabase: RoomDatabase() {
+    abstract val journalEntryDao: JournalEntryDao
 }
