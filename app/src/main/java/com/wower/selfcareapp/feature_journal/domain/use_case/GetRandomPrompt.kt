af9c9eadedf7f4a7dad80f7cf81b6f9d02e33cd1
Marta Wower <marta.wower24@gmail.com>
2025-04-08 16:27:04 -0600

    GetRandomPrompt Use Case
    created GetRandomPrompt Use Case that gets random unused prompt and if all prompts are used it resets them and gets a random prompt again. Then it marks th eprompt as used.

---
+++ app/src/main/java/com/wower/selfcareapp/feature_journal/domain/use_case/GetRandomPrompt.kt
@@ -0,0 +1,18 @@
+package com.wower.selfcareapp.feature_journal.domain.use_case
+
+import com.wower.selfcareapp.feature_journal.domain.model.JournalPrompt
+import com.wower.selfcareapp.feature_journal.domain.repository.JournalPromptRepository
+
+class GetRandomPrompt(
+    private val repository: JournalPromptRepository
+) {
+    suspend operator fun invoke() : JournalPrompt? {
+        var prompt = repository.getRandomPrompt()
+        if (prompt == null) {
+            repository.resetPrompts()
+            prompt = repository.getRandomPrompt()
+        }
+        repository.markPromptAsUsed(prompt?.id ?: 0)
+        return prompt
+    }
+}
