23ff744182b118e071ffc1be8eefa4079552c61f
Marta Wower <marta.wower24@gmail.com>
2025-04-09 08:55:46 -0600

    Affirmation repository
    Added Affirmation Repository interface and class based on Journal Prompt repository

---
+++ app/src/main/java/com/wower/selfcareapp/feature_affirmation/data/repository/AffirmationRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.wower.selfcareapp.feature_affirmation.data.repository
+
+import com.wower.selfcareapp.feature_affirmation.data.data_source.AffirmationDao
+import com.wower.selfcareapp.feature_affirmation.domain.model.Affirmation
+import com.wower.selfcareapp.feature_affirmation.domain.repository.AffirmationRepository
+
+class AffirmationRepositoryImpl(
+    private val dao: AffirmationDao
+): AffirmationRepository {
+    override suspend fun insertAffirmation(affirmation: Affirmation) {
+        dao.insertAffirmation(affirmation)
+    }
+
+    override suspend fun getRandomAffirmation(): Affirmation? {
+        return dao.getRandomAffirmation()
+    }
+
+    override suspend fun markAffirmationAsUsed(affirmationId: Int) {
+        dao.markAffirmationAsUsed(affirmationId)
+    }
+
+    override suspend fun resetAffirmation() {
+        dao.resetAffirmations()
+    }
+}
