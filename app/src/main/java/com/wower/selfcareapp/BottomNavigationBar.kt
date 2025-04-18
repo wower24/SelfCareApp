82a6560482d44d7b06cb2723884eeee698128f68
Marta Wower <marta.wower24@gmail.com>
2025-04-18 15:55:04 -0600

    Bottom Bar Composable
    Added BottomNavigationBar Composable

---
+++ app/src/main/java/com/wower/selfcareapp/BottomNavigationBar.kt
@@ -0,0 +1,55 @@
+package com.wower.selfcareapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.wower.selfcareapp.util.BottomNavItem
+import com.wower.selfcareapp.util.Screen
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController,
+    items: List<BottomNavItem>
+) {
+    val selectedIndex = rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        items.forEachIndexed { index, destination ->
+            NavigationBarItem(
+                label = {
+                    Text(text = destination.label)
+                },
+                icon = {
+                    Icon(
+                        imageVector = destination.icon,
+                        contentDescription = destination.label
+                    )
+                },
+                selected = index == selectedIndex.value,
+                onClick = {
+                    selectedIndex.value = index
+                    navController.navigate(items[index].route) {
+                        popUpTo(Screen.JournalScreen.route)
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+}
