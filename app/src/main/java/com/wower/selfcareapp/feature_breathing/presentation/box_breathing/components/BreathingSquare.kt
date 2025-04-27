9080a3cb8b1e1edc62dbe08be204ea8e50e24cb8
Marta Wower <marta.wower24@gmail.com>
2025-04-27 12:12:29 -0600

    new approach - no glitches
    Changed animation to a circle moving around the square outline. That seems to remove glitches.

--- app/src/main/java/com/wower/selfcareapp/feature_breathing/presentation/box_breathing/components/BreathingSquare.kt
+++ app/src/main/java/com/wower/selfcareapp/feature_breathing/presentation/box_breathing/components/BreathingSquare.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wower.selfcareapp.feature_breathing.domain.model.BreathingPhase
@@ -31,89 +32,50 @@ import com.wower.selfcareapp.ui.theme.SelfCareColor
 fun BreathingSquare(
     phase: BreathingPhase
 ) {
+
     val progress = remember { Animatable(0f) }
 
-    LaunchedEffect(phase) {
-        progress.snapTo(0f)
-        progress.animateTo(
-            targetValue = 1f,
-            animationSpec = tween(durationMillis = 4000, easing = LinearEasing)
-        )
+    LaunchedEffect(Unit) {
+        while (true) {
+            progress.snapTo(0f)
+            progress.animateTo(
+                targetValue = 4f,
+                animationSpec = tween(durationMillis = 16000, easing = LinearEasing)
+            )
+        }
     }
 
     Canvas(modifier = Modifier.size(200.dp)) {
         val squareSize = size.minDimension
         val sideLength = squareSize
 
-        val start = Offset.Zero
-        val topRight = Offset(sideLength, 0f)
-        val bottomRight = Offset(sideLength, sideLength)
-        val bottomLeft = Offset(0f, sideLength)
+        drawRect(
+            color = SelfCareColor.DarkGreen,
+            topLeft = Offset.Zero,
+            alpha = 0.5f,
+            style = Stroke(10f),
+            size = size
+        )
 
-        if(phase == BreathingPhase.Inhale) {
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = start,
-                end = Offset(progress.value * sideLength, 0f),
-                strokeWidth = 10f
-            )
-        } else if(phase == BreathingPhase.HoldAfterInhale) {
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = start,
-                end = topRight,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.LightPink,
-                start = topRight,
-                end = Offset(sideLength, progress.value * sideLength),
-                strokeWidth = 10f
-            )
-        } else if(phase == BreathingPhase.Exhale) {
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = start,
-                end = topRight,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.LightPink,
-                start = topRight,
-                end = bottomRight,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = bottomRight,
-                end = Offset((sideLength - progress.value * sideLength), sideLength),
-                strokeWidth = 10f
-            )
-        } else {
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = start,
-                end = topRight,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.LightPink,
-                start = topRight,
-                end = bottomRight,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.DarkPink,
-                start = bottomRight,
-                end = bottomLeft,
-                strokeWidth = 10f
-            )
-            drawLine(
-                color = SelfCareColor.LightPink,
-                start = bottomLeft,
-                end = Offset(0f, sideLength - (progress.value * sideLength)),
-                strokeWidth = 10f
-            )
+        val currentPosition = when {
+            progress.value <= 1f -> {
+                Offset(progress.value * sideLength, 0f)
+            }
+            progress.value <= 2f -> {
+                Offset(sideLength, (progress.value - 1f) * sideLength)
+            }
+            progress.value <= 3f -> {
+                Offset(sideLength - (progress.value - 2f) * sideLength, sideLength)
+            }
+            else -> {
+                Offset(0f, sideLength - (progress.value - 3f) * sideLength)
+            }
         }
+
+        drawCircle(
+            color = SelfCareColor.DarkPink,
+            radius = 20f,
+            center = currentPosition
+        )
     }
 }
